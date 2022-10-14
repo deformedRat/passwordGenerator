e124ed1ec3044793b6b88200bddf8121ddc1df48
deformedRat <63128382+deformedRat@users.noreply.github.com>
2022-10-14 11:43:40 -0500

    Update Dinglebob.js

--- Dinglebob.js
+++ Dinglebob.js
@@ -38,7 +38,7 @@ function generateCapital(){
   //get rid of the decimals
   newLetter = Math.floor(newLetter)+65;
   console.log(newLetter)
-  passwordHolder.innerHTML = String.fromCharCode(newLetter);
+  passwordHolder.innerHTML = passwordHolder.innerHTML + String.fromCharCode(newLetter);
   }//end generate
 
 function generateLower(){
@@ -47,7 +47,7 @@ function generateLower(){
   //get rid of the decimals
   newLetter = Math.floor(newLetter)+97;
   console.log(newLetter)
-  passwordHolder.innerHTML = String.fromCharCode(newLetter);
+  passwordHolder.innerHTML = passwordHolder.innerHTML + String.fromCharCode(newLetter);
   }//end generate
 
 function generateNumber(){
@@ -56,7 +56,7 @@ function generateNumber(){
   //get rid of the decimals
   newLetter = Math.floor(newLetter)+48;
   console.log(newLetter)
-  passwordHolder.innerHTML = String.fromCharCode(newLetter);
+  passwordHolder.innerHTML = passwordHolder.innerHTML + String.fromCharCode(newLetter);
   }//end generate
 
 function generateSpecial(){
@@ -64,5 +64,28 @@ function generateSpecial(){
   var newLetter = Math.random()*newSpecial.length;
   newLetter = Math.floor(newLetter)
   console.log(newLetter)
-  passwordHolder.innerHTML = (newSpecial[newLetter])
+  passwordHolder.innerHTML = passwordHolder.innerHTML + (newSpecial[newLetter]);
   }//end generate
+
+function generatePassword(){
+  passwordHolder.innerHTML = ""
+  var tickValue
+  for(i=0 ; i<16 ; i++){
+  tickValue = Math.random()*4
+  tickValue = Math.floor(tickValue)
+    console.log(tickValue)
+    if (tickValue == 0) {
+    generateCapital();
+    } else {
+      if (tickValue == 1) {
+      generateLower();
+      } else {
+        if (tickValue == 2){
+          generateNumber();
+        } else {
+          generateSpecial();
+        }
+      }    
+  }
+  }//end loop
+}//end startHere
